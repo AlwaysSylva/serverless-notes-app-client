e038dfa294cb6a2fcd1768d32925a1df8223d607
Jon Hart <jonny.1000@gmail.com>
2020-03-07 10:39:21 +0000

    Add AWS amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "sylva-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://mv7t3usboj.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_FsOXNYlT4",
+    APP_CLIENT_ID: "6t5k9b77hlkq7ojtg5m4ipt2ap",
+    IDENTITY_POOL_ID: "eu-west-2:0c44a4ad-582f-400b-b011-6eb102fc96e7"
+  }
+};
